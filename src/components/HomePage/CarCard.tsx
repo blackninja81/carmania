e3335f7dc405bd2b6bb29b5861b50d0168f473cd
blackninja81 <johnkaburu81@gmail.com>
2023-05-17 22:53:22 +0300

    updated css stylings

--- src/components/HomePage/CarCard.tsx
+++ src/components/HomePage/CarCard.tsx
@@ -18,51 +18,57 @@ const CarCard = () => {
     //   glareMaxOpacity={0.45}
     //   scale={.9}
     // >
-      <div className="card-main">
-        <div className="card-image">
-          <Image src={image1} alt="car-image" />
+    <div className="card-main">
+      <div className="card-image">
+        <Image src={image1} alt="car-image" />
+      </div>
+      <div className="card-details">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae
+          non aliquid, distinctio, assumenda aspernatur doloribus quisquam
+          consectetur sapiente asperiores voluptate reprehenderit? Cumque nemo
+          nihil adipisci.
+        </p>
+        {/* <button className="enquire-button">
+          <h6>Enqiure</h6>
+          <div className="plus-button">
+            <BsPlusCircle />
+          </div>
+        </button>
+        <button className="details-button">
+          <h6>View Details</h6>
+          <div className="plus-button">
+            <BsPlusCircle />
+          </div>
+        </button> */}
+        <h3>Maclaren 720</h3>
+        <h3>
+          KSH <span>5,900,000.00</span>
+        </h3>
+      </div>
+      <div className="card-specs">
+        <div>
+          <AiOutlineArrowRight className="spec" />
+          CSL1165
         </div>
-        <div className="card-details">
-          <button className="enquire-button">
-            <h6>Enqiure</h6>
-            <div className="plus-button">
-              <BsPlusCircle />
-            </div>
-          </button>
-          <button className="details-button">
-            <h6>View Details</h6>
-            <div className="plus-button">
-              <BsPlusCircle />
-            </div>
-          </button>
-          <h3>Maclaren 720</h3>
-          <h3>
-            KSH <span>5,900,000.00</span>
-          </h3>
+        <div>
+          <TbEngine className="spec" />
+          3200
         </div>
-        <div className="card-specs">
-          <div>
-            <AiOutlineArrowRight className="spec" />
-            CSL1165
-          </div>
-          <div>
-            <TbEngine className="spec" />
-            3200
-          </div>
-          <div>
-            <GiGearStickPattern className="spec" />
-            Automatic
-          </div>
-          <div>
-            <GiGasPump className="spec" />
-            Diesel
-          </div>
-          <div>
-            <AiOutlineBgColors className="spec" />
-            White
-          </div>
+        <div>
+          <GiGearStickPattern className="spec" />
+          Automatic
+        </div>
+        <div>
+          <GiGasPump className="spec" />
+          Diesel
+        </div>
+        <div>
+          <AiOutlineBgColors className="spec" />
+          White
         </div>
       </div>
+    </div>
     // </Tilt>
   );
 };
